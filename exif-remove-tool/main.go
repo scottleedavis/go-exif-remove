fa41d42b02d6684fc879202e84b581d2b4156d46
Scott Lee Davis <skawtus@gmail.com>
2019-09-01 18:51:30 -0700

    module changes

---
+++ exif-remove-tool/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/dsoprea/go-exif"
+	"github.com/dsoprea/go-jpeg-image-structure"
+	"github.com/dsoprea/go-png-image-structure"
+	"github.com/scottleedavis/go-exif-remove"
+)
+
+func main() {
+
+	if len(os.Args) == 1 {
+		var files []string
+		root := "img"
+		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+			if path != "img" {
+				files = append(files, path)
+			}
+			return nil
+		})
+		if err != nil {
+			panic(err)
+		}
+		pass := 0
+		fail := 0
+		for _, file := range files {
+			fmt.Println(file)
+			if b, err := handleFile(file); err != nil {
+				fail += 1
+			} else {
+				pass += 1
+				f := filepath.Base(file)
+				ioutil.WriteFile("img_output/"+f, b, 0644)
+			}
+			fmt.Println()
+		}
+
+		percentage := 100 * pass / (pass + fail)
+		fmt.Printf("Results (%v%%): %v pass, %v fail \n", int(percentage), pass, fail)
+	} else {
+		path := os.Args[1]
+		if b, err := handleFile(path); err != nil {
+			fmt.Printf(err.Error())
+		} else {
+			file := filepath.Base(path)
+			ioutil.WriteFile("img_output/"+file, b, 0644)
+		}
+	}
+
+}
+
+func handleFile(filepath string) ([]byte, error) {
+	if data, err := ioutil.ReadFile(filepath); err != nil {
+		fmt.Printf(err.Error())
+		return nil, err
+	} else {
+		_, err = jpeg.Decode(bytes.NewReader(data))
+		if err != nil {
+			fmt.Printf("ERROR: original image is corrupt" + err.Error() + "\n")
+			return nil, err
+		}
+		filtered, err := extractEXIF(data)
+		if err != nil {
+			if !strings.EqualFold(err.Error(), "no exif data") {
+				fmt.Printf("* " + err.Error() + "\n")
+				return nil, errors.New(err.Error())
+			}
+		}
+		return filtered, nil
+	}
+}
--- exif_remove.go
+++ exif_remove.go
@@ -1,15 +1,9 @@
-package main
+package exifremove
 
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"image"
-	"image/jpeg"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-	"strings"
 
 	"github.com/dsoprea/go-exif"
 	"github.com/dsoprea/go-jpeg-image-structure"
@@ -31,70 +25,7 @@ type MediaContext struct {
 	Media     interface{}
 }
 
-func main() {
-
-	if len(os.Args) == 1 {
-		var files []string
-		root := "img"
-		err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-			if path != "img" {
-				files = append(files, path)
-			}
-			return nil
-		})
-		if err != nil {
-			panic(err)
-		}
-		pass := 0
-		fail := 0
-		for _, file := range files {
-			fmt.Println(file)
-			if b, err := handleFile(file); err != nil {
-				fail += 1
-			} else {
-				pass += 1
-				f := filepath.Base(file)
-				ioutil.WriteFile("img_output/"+f, b, 0644)
-			}
-			fmt.Println()
-		}
-
-		percentage := 100 * pass / (pass + fail)
-		fmt.Printf("Results (%v%%): %v pass, %v fail \n", int(percentage), pass, fail)
-	} else {
-		path := os.Args[1]
-		if b, err := handleFile(path); err != nil {
-			fmt.Printf(err.Error())
-		} else {
-			file := filepath.Base(path)
-			ioutil.WriteFile("img_output/"+file, b, 0644)
-		}
-	}
-
-}
-
-func handleFile(filepath string) ([]byte, error) {
-	if data, err := ioutil.ReadFile(filepath); err != nil {
-		fmt.Printf(err.Error())
-		return nil, err
-	} else {
-		_, err = jpeg.Decode(bytes.NewReader(data))
-		if err != nil {
-			fmt.Printf("ERROR: original image is corrupt" + err.Error() + "\n")
-			return nil, err
-		}
-		filtered, err := extractEXIF(data)
-		if err != nil {
-			if !strings.EqualFold(err.Error(), "no exif data") {
-				fmt.Printf("* " + err.Error() + "\n")
-				return nil, errors.New(err.Error())
-			}
-		}
-		return filtered, nil
-	}
-}
-
-func extractEXIF(data []byte) ([]byte, error) {
+func RemoveEXIF(data []byte) ([]byte, error) {
 	jmp := jpegstructure.NewJpegMediaParser()
 	pmp := pngstructure.NewPngMediaParser()
 	mc := &MediaContext{
