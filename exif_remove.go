1c628a4846c5e8993d7b9ae5137123a4b5267d7e
Scott Lee Davis <skawtus@gmail.com>
2019-09-01 19:50:08 -0700

    getting PNG support started

--- exif_remove.go
+++ exif_remove.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"errors"
 	"image"
+	"fmt"
 
 	"github.com/dsoprea/go-exif"
 	"github.com/dsoprea/go-jpeg-image-structure"
@@ -37,6 +38,8 @@ func RemoveEXIF(data []byte) ([]byte, error) {
 	filtered := []byte{}
 
 	if jmp.LooksLikeFormat(data) {
+		fmt.Printf("JPG\n")
+
 		mc.MediaType = JpegMediaType
 		sl, err := jmp.ParseBytes(data)
 		if err != nil {
@@ -80,6 +83,47 @@ func RemoveEXIF(data []byte) ([]byte, error) {
 
 	} else if pmp.LooksLikeFormat(data) {
 		mc.MediaType = PngMediaType
+		fmt.Printf("PNG\n")
+
+		cs, err := pmp.ParseBytes(data)
+		if err != nil {
+			return nil, err
+		}
+		mc.Media = cs
+
+		if rootIfd, rawExif, err := cs.Exif(); err != nil {
+			return nil, err
+		} else {
+			mc.RootIfd = rootIfd
+			mc.RawExif = rawExif
+		}
+		if _, err := cs.FindExif(); err != nil {
+			return nil, err
+		} else {
+
+			startExifBytes := StartBytes
+			endExifBytes := EndBytes
+			print("wow\n")
+			if bytes.Contains(data, mc.RawExif) {
+				fmt.Printf("hi\n")
+				for i := 0; i < len(data)-len(mc.RawExif); i++ {
+					if bytes.Compare(data[i:i+len(mc.RawExif)], mc.RawExif) == 0 {
+						startExifBytes = i
+						endExifBytes = i + len(mc.RawExif)
+					}
+				}
+				fill := make([]byte, len(data[startExifBytes:endExifBytes]))
+				copy(data[startExifBytes:endExifBytes], fill)
+			}
+
+			filtered = data
+
+			_, _, err = image.Decode(bytes.NewReader(filtered))
+			if err != nil {
+				return nil, errors.New("EXIF removal corrupted " + err.Error())
+			}
+
+		}
 	}
 
 	return filtered, nil
