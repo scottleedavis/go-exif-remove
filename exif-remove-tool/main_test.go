aba76f80677a4fe3ce66528a064652910080c190
Scott Lee Davis <skawtus@gmail.com>
2019-09-02 09:47:02 -0700

    exif-remove-tool test

---
+++ exif-remove-tool/main_test.go
@@ -0,0 +1,31 @@
+package main
+import (
+	"fmt"
+	"bytes"
+	"os/exec"
+	"regexp"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMain(t *testing.T) {
+	cmd := exec.Command(
+		"go", "run", "main.go")
+
+	b := new(bytes.Buffer)
+	cmd.Stdout = b
+	cmd.Stderr = b
+
+	err := cmd.Run()
+	actualRaw := b.Bytes()
+
+	assert.Nil(t, err)
+
+	fmt.Printf("%v "+string(actualRaw), len(actualRaw))
+
+	var re = regexp.MustCompile(`0 fail`)
+	matches := re.FindStringSubmatch(string(actualRaw))
+	assert.True(t, len(matches) == 1, "Should parse all files in img folder")
+
+}
