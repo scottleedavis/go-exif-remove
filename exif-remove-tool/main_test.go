cd37b679f97d8f1e777b2d1dcbb8cf0b2fe5b1b4
Scott Lee Davis <skawtus@gmail.com>
2019-09-07 19:06:03 -0700

    update main test

--- exif-remove-tool/main_test.go
+++ exif-remove-tool/main_test.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"bytes"
-	"fmt"
+	"os"
 	"os/exec"
 	"regexp"
 	"testing"
@@ -10,7 +10,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMain(t *testing.T) {
+func TestMain(m *testing.M) {
+	code := m.Run()
+	os.Exit(code)
+}
+
+func TestImages(t *testing.T) {
 	cmd := exec.Command(
 		"go", "run", "main.go")
 
@@ -23,10 +28,7 @@ func TestMain(t *testing.T) {
 
 	assert.Nil(t, err)
 
-	fmt.Printf("%v "+string(actualRaw), len(actualRaw))
-
 	var re = regexp.MustCompile(`0 fail`)
 	matches := re.FindStringSubmatch(string(actualRaw))
 	assert.True(t, len(matches) == 1, "Should parse all files in img folder")
-
 }
